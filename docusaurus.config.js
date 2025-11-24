597ac3b53889da7d2e8834c7b02b41b8361ba1fd
Guilherme <a89970@ualg.pt>
2025-11-24 15:09:56 +0000

    Configuração

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,9 +27,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'gafonso06', // Usually your GitHub org/user name.
+  projectName: 'dokku', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
